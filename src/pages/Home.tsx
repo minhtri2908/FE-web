70e20e66bd12758c7eed09d9fefa3358d48d9ae2
Phạm Minh Trí <91834683+minhtri2908@users.noreply.github.com>
2025-07-14 15:15:58 +0700

    update version tsc

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 const aboutData = {
   avatar: "/images/IMG_9261-3.jpg",
